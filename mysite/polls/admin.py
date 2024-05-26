9373ee1a1144655bf93cd4cd59dce7a3611406a6
lmocs <loganmoreno.cs@gmail.com>
2024-05-25 23:17:15 -0400

    Commit second part.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Question # This is added to add Question objects to admin interface
+
 # Register your models here.
+admin.site.register(Question)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,24 @@
+# Creating models allows Django to create a datase schema and an API to access Question and Choice objects
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date pubbed")
+
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # This relates another model to this model
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
